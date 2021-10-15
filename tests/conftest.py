2a5ea92c9d930908d5b9ad04b63f14f84a2d6e8e
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-10-15 21:29:55 +0200

    Tests for logout endpoint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
+import collections
 import os
 
 import pytest
-from flask import Response
+from flask import url_for
 from pytest_factoryboy import register
-from werkzeug.utils import cached_property
 
 from wk.app import create_app
 from wk.db import database, models
@@ -11,14 +11,6 @@ from wk.db import database, models
 from .factories import UserFactory
 
 register(UserFactory)
-
-
-class TestResponse(Response):
-    @cached_property
-    def text(self):
-        if self.mimetype.startswith("text"):
-            return self.data.decode(self.charset)
-        return self.data
 
 
 def fake_gen_password_hash(password):
@@ -29,9 +21,43 @@ def fake_check_password_hash(stored, password):
     return stored == password
 
 
+Tokens = collections.namedtuple(
+    "Tokens",
+    ["csrf_access_token", "csrf_refresh_token", "access_token", "refresh_token"],
+)
+
+
 @pytest.fixture(scope="session", autouse=True)
 def faker_session_locale():
     return ["pl_PL"]
+
+
+def _cookie_from_response(response, cookie_name):
+    cookie_headers = response.headers.getlist("Set-Cookie")
+    for header in cookie_headers:
+        attributes = header.split(";")
+        if cookie_name in attributes[0]:
+            cookie = {}
+            for attr in attributes:
+                split = attr.split("=")
+                cookie[split[0].strip().lower()] = split[1] if len(split) > 1 else True
+            return cookie
+    return None
+
+
+@pytest.fixture()
+def login():
+    def _login(client, email, password):
+        url = url_for("auth.login")
+        rv = client.post(url, json={"email": email, "password": password})
+        return Tokens(
+            csrf_access_token=_cookie_from_response(rv, "csrf_access_token"),
+            csrf_refresh_token=_cookie_from_response(rv, "csrf_refresh_token"),
+            access_token=_cookie_from_response(rv, "access_token"),
+            refresh_token=_cookie_from_response(rv, "refresh_token"),
+        )
+
+    return _login
 
 
 @pytest.fixture()
@@ -44,7 +70,6 @@ def app(mocker):
         {"WK_SECRET_KEY": "very secret", "WK_JWT_SECRET_KEY": "another very secret"},
     )
     app = create_app(testing=True)
-    app.response_class = TestResponse
     with app.app_context():
         database.create_tables(models)
         yield app
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -67,3 +67,35 @@ def test_login_fail_user_not_found(client):
     rv = client.post(url, json=input_data)
     assert rv.status_code == 400
     assert "wrong credentials" in rv.json["message"]
+
+
+def test_logout(client, login, user_factory):
+    email = "user@example.com"
+    password = "somepassword"
+    user_factory(email=email, password=password)
+    tokens = login(client, email, password)
+    headers = {"X-CSRF-TOKEN": tokens.csrf_refresh_token["csrf_refresh_token"]}
+    url = url_for("auth.logout")
+    rv = client.post(url, headers=headers)
+    assert rv.status_code == 200
+
+
+def test_logout_bad_token(client, login, user_factory):
+    email = "user@example.com"
+    password = "somepassword"
+    user_factory(email=email, password=password)
+    login(client, email, password)
+    headers = {"X-CSRF-TOKEN": "wrong"}
+    url = url_for("auth.logout")
+    rv = client.post(url, headers=headers)
+    assert rv.status_code == 401
+
+
+def test_logout_missing_token(client, login, user_factory):
+    email = "user@example.com"
+    password = "somepassword"
+    user_factory(email=email, password=password)
+    login(client, email, password)
+    url = url_for("auth.logout")
+    rv = client.post(url)
+    assert rv.status_code == 401
