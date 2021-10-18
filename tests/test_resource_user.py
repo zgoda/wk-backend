df88d52c1e8eb2cc009488cddc1ce5455b04aa66
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-10-18 12:57:16 +0200

    Tests for user item resource

---
+++ tests/test_resource_user.py
@@ -0,0 +1,42 @@
+from flask import url_for
+
+
+def test_user_modify(client, login, user_factory):
+    email = "test@example.com"
+    password = "pass"
+    name = "test name"
+    user_factory(email=email, password=password, name=name)
+    tokens = login(client, email, password)
+    headers = {"X-CSRF-TOKEN": tokens.csrf_access_token["csrf_access_token"]}
+    url = url_for("api.user_item", email=email)
+    data = {"name": "My Fancy Name"}
+    rv = client.post(url, json=data, headers=headers)
+    assert rv.status_code == 200
+    assert rv.json["user"]["name"] == data["name"]
+
+
+def test_user_modify_fail_notfound(client, login, user_factory):
+    email = "test@example.com"
+    another_email = "another@example.com"
+    password = "pass"
+    user_factory(email=email, password=password)
+    tokens = login(client, email, password)
+    headers = {"X-CSRF-TOKEN": tokens.csrf_access_token["csrf_access_token"]}
+    url = url_for("api.user_item", email=another_email)
+    data = {"name": "My Fancy Name"}
+    rv = client.post(url, json=data, headers=headers)
+    assert rv.status_code == 404
+
+
+def test_user_modify_fail_notallowed(client, login, user_factory):
+    email = "test@example.com"
+    another_email = "another@example.com"
+    password = "pass"
+    user_factory(email=email, password=password)
+    user_factory(email=another_email, password=password)
+    tokens = login(client, email, password)
+    headers = {"X-CSRF-TOKEN": tokens.csrf_access_token["csrf_access_token"]}
+    url = url_for("api.user_item", email=another_email)
+    data = {"name": "My Fancy Name"}
+    rv = client.post(url, json=data, headers=headers)
+    assert rv.status_code == 403
